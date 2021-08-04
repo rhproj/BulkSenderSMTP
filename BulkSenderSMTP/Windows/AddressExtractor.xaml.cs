8851b2bc4edcb8a3a1638713cffac341a1f7e7e2
Rh Proj <r.proj@hotmail.com>
2021-08-04 13:53:59 +0300

    Add project files.

---
+++ BulkSenderSMTP/Windows/AddressExtractor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BulkSenderSMTP.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddressExtractor.xaml
+    /// </summary>
+    public partial class AddressExtractor : Window
+    {
+        public AddressExtractor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BulkSenderSMTP/Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BulkSenderSMTP.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BulkSenderSMTP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> emailList = new List<string>();
+        List<string> messageList = new List<string>();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_extractView_Click(object sender, RoutedEventArgs e)
+        {
+            AddressExtractor addressExtractor = new AddressExtractor();
+            addressExtractor.Owner = this;
+            addressExtractor.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            addressExtractor.Show();
+        }
+    }
+}
