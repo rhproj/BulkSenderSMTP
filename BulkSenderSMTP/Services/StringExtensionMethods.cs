00a629c93c1ab2fea9b31ec0d73c0c244ff16fa9
Rh Proj <r.proj@hotmail.com>
2021-08-04 14:39:23 +0300

    AddressExtractor

---
+++ BulkSenderSMTP/Services/StringExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BulkSenderSMTP.Services
+{
+    public static class StringExtensionMethods
+    {
+        public static List<string> EverythingBetween(this string source, string start, string end)
+        {
+            var results = new List<string>();
+
+            string pattern = string.Format(
+                "{0}({1}){2}",
+                Regex.Escape(start),
+                ".+?",
+                 Regex.Escape(end));
+
+            foreach (Match m in Regex.Matches(source, pattern))
+            {
+                results.Add(m.Groups[1].Value);
+            }
+
+            return results;
+        }
+    }
+}
--- BulkSenderSMTP/Windows/AddressExtractor.xaml.cs
+++ BulkSenderSMTP/Windows/AddressExtractor.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BulkSenderSMTP.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,30 @@ namespace BulkSenderSMTP.Windows
         public AddressExtractor()
         {
             InitializeComponent();
+            //ComboDelimiterFiller();
+        }
+
+        //private void ComboDelimiterFiller()
+        //{
+        //    comboDelimiter.Items.Add("<>");
+        //    comboDelimiter.Items.Add(@"""");
+        //    comboDelimiter.Items.Add("{}");
+        //    comboDelimiter.Items.Add("[]");
+        //    comboDelimiter.Items.Add("()");
+        //}
+
+
+
+        private void btnExtract_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> mailList = tbRawAddresses.Text.EverythingBetween(tbStartDelim.Text, tbEndDelim.Text);
+
+            tbRawAddresses.Text = string.Join(Environment.NewLine,mailList);
+
+            //foreach (var adress in results)
+            //{
+            //    listBoxEMails.Items.Add(adress);
+            //}
         }
     }
 }
