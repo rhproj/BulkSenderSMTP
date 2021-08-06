a8848ce7cc8ce1e08ed3e71a44404369291cb838
Rh Proj <r.proj@hotmail.com>
2021-08-06 16:42:26 +0300

    b/g

--- BulkSenderSMTP/Windows/MainWindow.xaml.cs
+++ BulkSenderSMTP/Windows/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Win32;
 using MimeKit;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -16,12 +17,43 @@ namespace BulkSenderSMTP
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> emailList = new List<string>();
-        List<string> messageList = new List<string>();
+        List<string> emailList; 
+        List<string> messageList;
+        BackgroundWorker backgroundWorker = new BackgroundWorker();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+
+            backgroundWorker.WorkerReportsProgress = true;
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ImgSend.Visibility = Visibility.Visible;
+            btn_Send.IsEnabled = false;
+
+            lblProgress.Content = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Dispatcher.Invoke(
+                new Action(() =>
+                {
+                    BulkSend();
+                }));
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ImgSend.Visibility = Visibility.Hidden;
+            btn_Send.IsEnabled = true;
+            MessageBox.Show("Done");
         }
 
         private void btn_extractView_Click(object sender, RoutedEventArgs e)
@@ -40,6 +72,9 @@ namespace BulkSenderSMTP
 
             if (ofd.ShowDialog() == true)
             {
+                emailList = new List<string>();
+                messageList = new List<string>();
+
                 using (var sR = new StreamReader(ofd.FileName, Encoding.UTF8))
                 {
                     while (!sR.EndOfStream)
@@ -57,13 +92,27 @@ namespace BulkSenderSMTP
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-            ImgSend.Visibility = Visibility.Visible;
+            #region T T
+            //new Task(BulkSend).Start();
 
-            BulkSend();
+            //Thread thread = new Thread(delegate ()
+            //{
+            //    BulkSend();
+            //});
+            //thread.IsBackground = true;
+            //thread.Start(); 
+            #endregion
 
+            if (!backgroundWorker.IsBusy) //если bgW не занят:
+            {
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+
+        private void BulkSend()
+        {
             #region wo-BGw
-            //CheckForIllegalCrossThreadCalls = false;
-
             //declare the smtp object
             SmtpClient client = new SmtpClient();
             client.Timeout = 300000;
@@ -87,41 +136,56 @@ namespace BulkSenderSMTP
                 mail.To.Add(new MailboxAddress(emailList[i])); //listBoxEMails.Items[i].ToString()));
                 client.Send(mail);
 
-                //Thread.Sleep(1000);
+                backgroundWorker.ReportProgress(i);
+
+                Thread.Sleep(1000); ///TEST
             }
-
-            #region BILO
-            //for (int i = 0; i < emailList.Count; i++)
-            //{
-            //    BodyBuilder builder = new BodyBuilder(); //Builds HTML body for sending
-            //    builder.HtmlBody = messageList[i].ToString();
-
-            //    //Define the mail headers
-            //    MimeMessage mail = new MimeMessage();
-            //    mail.Subject = tbSubject.Text;
-            //    mail.Body = builder.ToMessageBody();
-
-            //    client.Connect(tbServer.Text, int.Parse(tbPort.Text)); // cbSSL.Checked
-            //    client.Authenticate(tbLogin.Text, pbPassword.Password);
-
-            //    mail.From.Add(new MailboxAddress(tbFrom.Text, tbLogin.Text));
-            //    mail.To.Add(new MailboxAddress(emailList[i])); //listBoxEMails.Items[i].ToString()));
-            //    client.Send(mail);
-
-            //    Thread.Sleep(1000);
-            //} 
-            #endregion
 
             client.Disconnect(true);
             #endregion
-
-            ImgSend.Visibility = Visibility.Hidden;
-            MessageBox.Show("Done");
         }
 
-        private void BulkSend()
-        {
-            throw new NotImplementedException();
-        }
+        //private void BulkSend()
+        //{
+        //    //Dispatcher.BeginInvoke(new Action(() =>
+        //    //{
+        //    //    ImgSend.Visibility = Visibility.Visible;
+        //    //}));
+
+        //    #region wo-BGw
+        //    //declare the smtp object
+        //    SmtpClient client = new SmtpClient();
+        //    client.Timeout = 300000;
+        //    client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+        //    BodyBuilder builder = new BodyBuilder(); //Builds HTML body for sending
+        //                                             //Define the mail headers
+        //    MimeMessage mail = new MimeMessage();
+        //    mail.Subject = tbSubject.Text;
+
+        //    client.Connect(tbServer.Text, int.Parse(tbPort.Text)); // cbSSL.Checked
+        //    client.Authenticate(tbLogin.Text, pbPassword.Password);
+
+        //    for (int i = 0; i < emailList.Count; i++)
+        //    {
+        //        builder.HtmlBody = messageList[i].ToString();
+
+        //        mail.Body = builder.ToMessageBody();
+
+        //        mail.From.Add(new MailboxAddress(tbFrom.Text, tbLogin.Text));
+        //        mail.To.Add(new MailboxAddress(emailList[i])); //listBoxEMails.Items[i].ToString()));
+        //        client.Send(mail);
+
+        //        Thread.Sleep(1000); ///TEST
+        //    }
+
+        //    client.Disconnect(true);
+        //    #endregion
+
+        //    //Dispatcher.BeginInvoke((Action)(() =>
+        //    //{
+        //    //    ImgSend.Visibility = Visibility.Hidden;
+        //    //}));
+        //}
     }
 }
