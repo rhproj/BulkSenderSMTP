7399c4b4e9aa336f34a8db777022cccb3ea79a7b
Rh Proj <r.proj@hotmail.com>
2021-10-14 12:59:54 +0300

    Scrollviewer added

--- BulkSenderSMTP/Windows/MainWindow.xaml.cs
+++ BulkSenderSMTP/Windows/MainWindow.xaml.cs
@@ -5,11 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using System.Linq;
 
 namespace BulkSenderSMTP
 {
